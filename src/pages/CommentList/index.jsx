8fbd913a8287c68b5f309fea8605e2bd78dd8d0c
duanjiahong <buqingyunyou@163.com>
2022-04-21 18:17:03 +0800

    无评论时展示空状态提示

--- src/pages/CommentList/index.jsx
+++ src/pages/CommentList/index.jsx
@@ -10,11 +10,16 @@ export default class CommentList extends React.Component {
 
   render() {
     const { comments, deleteComment } = this.props;
+    // 判断显隐
+    const display = comments.length ? "none" : "block";
+
     return (
       <div className="commentList">
         {/* 评论回复展示块 */}
         <div className="col-md-8">
           <p className="title">评论回复:</p>
+
+          {/* 有评论时： */}
           <ul className="content">
             {comments.map((item) => {
               return (
@@ -26,6 +31,11 @@ export default class CommentList extends React.Component {
               );
             })}
           </ul>
+
+          {/* 无评论时： */}
+          <div style={{ display }} className="empty">
+            暂无评论，欢迎添加评论！
+          </div>
         </div>
       </div>
     );
